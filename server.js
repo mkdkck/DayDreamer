97f4fe33460ae776247d79fa81ae9927c6871ab6
Colin Liu <colin173@gmail.com>
2023-12-20 01:01:26 +0800

    fixed api routes issues, added handlebars-expressions, website is online

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 const express = require('express');          // Express framework for creating the server
 const session = require('express-session');  // Express-session for handling sessions
 const passport = require('./config/passport'); // Passport for authentication
+const exphbs = require('express-handlebars');
+
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 3000;      // Port configuration (use environment variable or default to 3000)
@@ -19,8 +21,13 @@ app.use(express.json());                     // Middleware to parse JSON bodies
 // Static directory
 app.use(express.static('public'));           // Setting up the public directory for static files
 
+const hbs = exphbs.create({});
+
 // Setting up session with a secret key
 app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // Passport initialization
 app.use(passport.initialize());
@@ -28,11 +35,11 @@ app.use(passport.session());
 
 // Importing routes
 const htmlRoutes = require('./routes/html'); // HTML routes for the application
-// const apiRoutes = require('./routes/api');   // API routes for the application
+const apiRoutes = require('./routes/api');   // API routes for the application
 
 // Registering routes
 app.use(htmlRoutes);                         // Use the HTML routes
-// app.use('/api', apiRoutes);                  // Use the API routes, prefixed with '/api'
+app.use('/api', apiRoutes);                  // Use the API routes, prefixed with '/api'
 
 // Syncing the Sequelize models and then starting the Express app
 db.sequelize.sync({ force: false }).then(() => {
