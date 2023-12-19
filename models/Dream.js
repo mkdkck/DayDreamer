f86beb0f221582cf0b2cf292907b313c2917c5e1
Peter Sadauskas <peter.sadauskas@gmail.com>
2023-12-19 21:19:45 +1030

    Fixed the define issue in Dream.js.

--- models/Dream.js
+++ models/Dream.js
@@ -1,36 +1,35 @@
 // Dream.js
 
-// Importing the Sequelize instance and DataTypes from the models folder
-const { sequelize, DataTypes } = require('./index'); // Adjust the path as necessary
+// This function will define the Dream model/schema
+module.exports = function(sequelize, DataTypes) {
+    const Dream = sequelize.define('Dream', {
+        // Defining the fields of the Dream model
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+            // This field will act as a foreign key linking to the User model
+        }
+    }, {
+        timestamps: true,
+        freezeTableName: true
+    });
 
-// Defining the Dream model/schema
-const Dream = sequelize.define('Dream', {
-    // Defining the fields of the Dream model
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    description: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-        // This field will act as a foreign key linking to the User model
-    }
-}, {
-    timestamps: true,
-    freezeTableName: true
-});
+    // Assuming there is a User model, this will link Dream to a User
+    Dream.belongsTo(require('./User')(sequelize, DataTypes), { foreignKey: 'user_id' });
 
-// Establishing relationships
-// Assuming there is a User model, this will link Dream to a User
-Dream.belongsTo(require('./User'), { foreignKey: 'user_id' });
-
-module.exports = Dream;
+    // Returning the Dream model
+    return Dream;
+};
