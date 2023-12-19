2c1319667e0fdb5aadadbd9f157ce6b27c01740b
Peter Sadauskas <peter.sadauskas@gmail.com>
2023-12-19 16:09:56 +1030

    Added base files and code per project plan.

---
+++ models/User.js
@@ -0,0 +1,48 @@
+// User.js
+
+// Importing the Sequelize library
+const Sequelize = require('sequelize');
+const bcrypt = require('bcrypt'); // bcrypt will be used for hashing passwords
+
+// This function will define the User model/schema
+module.exports = function(sequelize, DataTypes) {
+    const User = sequelize.define('User', {
+        // Defining fields of the User model
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true // Validates that the string is an email address
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    // A hook that will be executed before a User record is created,
+    // used here to hash the user's password
+    User.beforeCreate(user => {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    });
+
+    // A method to check if an unhashed password entered by the user can be compared to the hashed password stored in the database
+    User.prototype.validPassword = function(password) {
+        return bcrypt.compareSync(password, this.password);
+    };
+
+    // Returning the User model
+    return User;
+};
--- server.js
+++ server.js
@@ -1,42 +1,42 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+// server.js
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// Importing necessary modules
+const express = require('express');          // Express framework for creating the server
+const session = require('express-session');  // Express-session for handling sessions
+const passport = require('./config/passport'); // Passport for authentication
 
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 3000;      // Port configuration (use environment variable or default to 3000)
+const db = require('./models');             // Importing the Sequelize models
 
+// Creating the Express app
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-const sess = {
-  // secret: 'Super secret secret',
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+// Middleware for data parsing
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
+app.use(express.json());                     // Middleware to parse JSON bodies
 
-app.use(session(sess));
+// Static directory
+app.use(express.static('public'));           // Setting up the public directory for static files
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// Setting up session with a secret key
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// Passport initialization
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(routes);
+// Importing routes
+const htmlRoutes = require('./routes/html'); // HTML routes for the application
+const apiRoutes = require('./routes/api');   // API routes for the application
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+// Registering routes
+app.use(htmlRoutes);                         // Use the HTML routes
+app.use('/api', apiRoutes);                  // Use the API routes, prefixed with '/api'
+
+// Syncing the Sequelize models and then starting the Express app
+db.sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`App listening on PORT ${PORT}`);
+    });
 });
