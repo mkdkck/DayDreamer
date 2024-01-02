cebe4a885651420bbea349854ef44ee4685b6c34
Colin Liu <colin173@gmail.com>
2024-01-02 17:29:59 +0800

    change the config file

--- config/config.js
+++ config/config.js
@@ -1,15 +1,15 @@
 module.exports = {
     development: {
-        "username": "root", 
-        "database": "daydreamer_db", 
+        username: "root", 
+        database: "daydreamer_db", 
         password: process.env.DB_PASSWORD,
-        "host": "127.0.0.1", 
-        "dialect": "mysql", 
-        "logging": false
+        host: "127.0.0.1", 
+        dialect: "mysql", 
+        logging: false
     },
     production: {
         use_env_variable: "DATABASE_URL", // Use the DATABASE_URL environment variable provided by Heroku
-        dialect: "mysql",
+        dialect: "postgres",
         logging: false,
       },
 }
