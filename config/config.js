639c40e9c95e328420e7b7a1be31d65552c8c6c3
Colin Liu <colin173@gmail.com>
2024-01-04 09:25:16 +0800

    testing

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 module.exports= {
-  // "development": {
-  //   "database": "daydreamer_db",
-  //   "username": 'root',
-  //   "password": process.env.DB_PASSWORD,
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql",
-  //   "logging": false
-  // },
+  "development": {
+    "database": "daydreamer_db",
+    "username": 'root',
+    "password": process.env.DB_PASSWORD,
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "logging": false
+  },
     "production": {
       "use_env_variable": "JAWSDB_URL",
       "dialect": "mysql",
